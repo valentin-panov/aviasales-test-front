f7a045d108b496d288158537caeb5789d7101765
Valentin Panov <valentin.panov@me.com>
2022-04-04 01:06:41 +0300

    feat(src/components/Output/TicketList/TicketList.tsx) logic done

--- src/components/Output/TicketList/TicketList.tsx
+++ src/components/Output/TicketList/TicketList.tsx
@@ -10,60 +10,62 @@ export type Props = {
   className?: string
 }
 
-export const TicketList = memo<Props>(({ className }) => {
+export const TicketList = memo<Props>(({className}) => {
   const filters = useSelector((store: RootState) => store.filters, shallowEqual)
   const ticketsStore = useSelector(
-    (store: RootState) => store.tickets.tickets,
-    shallowEqual
+      (store: RootState) => store.tickets.tickets,
+      shallowEqual
   )
 
-  // console.log(
-  //   filters.filter,
-  //   Object.values(filters.filter).indexOf(false) === -1
-  // )
+  const filterArray: number[] = Object.values(filters.filter).reduce((newObj, value, idx) => {
+    if (value) {
+      newObj.push(idx)
+    }
+    return newObj
+  }, [])
 
   let tickets: InTicket[] =
-    Object.values(filters.filter).indexOf(false) === -1
-      ? [...ticketsStore]
-      : [...ticketsStore].filter(
-          (el) =>
-            el.segments[0].stops.length !== 2 &&
-            el.segments[1].stops.length !== 2
-        )
+      filterArray.length === 4
+          ? [...ticketsStore]
+          : [...ticketsStore].filter(
+              (el) =>
+                  filterArray.includes(el.segments[0].stops.length) &&
+                  filterArray.includes(el.segments[1].stops.length)
+          )
 
   if (filters.filter) {
   }
 
   if (filters.sort === 'optimal') {
     tickets.sort((a, b) =>
-      a.segments[0].stops.length + a.segments[1].stops.length >
-      b.segments[0].stops.length + b.segments[1].stops.length
-        ? 1
-        : b.segments[0].stops.length + b.segments[1].stops.length >
-          a.segments[0].stops.length + a.segments[1].stops.length
-        ? -1
-        : 0
+        a.segments[0].stops.length + a.segments[1].stops.length >
+        b.segments[0].stops.length + b.segments[1].stops.length
+            ? 1
+            : b.segments[0].stops.length + b.segments[1].stops.length >
+            a.segments[0].stops.length + a.segments[1].stops.length
+                ? -1
+                : 0
     )
   } else if (filters.sort === 'time') {
     tickets.sort((a, b) =>
-      a.segments[0].duration + a.segments[1].duration >
-      b.segments[0].duration + b.segments[1].duration
-        ? 1
-        : b.segments[0].duration + b.segments[1].duration >
-          a.segments[0].duration + a.segments[1].duration
-        ? -1
-        : 0
+        a.segments[0].duration + a.segments[1].duration >
+        b.segments[0].duration + b.segments[1].duration
+            ? 1
+            : b.segments[0].duration + b.segments[1].duration >
+            a.segments[0].duration + a.segments[1].duration
+                ? -1
+                : 0
     )
   } else {
     tickets.sort((a, b) => (a.price > b.price ? 1 : b.price > a.price ? -1 : 0))
   }
 
   return (
-    <div className={cn(s.root, className)}>
-      {tickets.slice(0, filters.limit).map((el) => (
-        <TicketCard key={`${el.price}${el.carrier}`} ticket={el} />
-      ))}
-    </div>
+      <div className={cn(s.root, className)}>
+        {tickets.slice(0, filters.limit).map((el) => (
+            <TicketCard key={`${el.price}${el.carrier}`} ticket={el}/>
+        ))}
+      </div>
   )
 })
 
